8497ca3677f8ff27ec7b83c3baea5fca3533afd9
jesusalvarez0210 <69257199+jesusalvarez0210@users.noreply.github.com>
2021-04-05 11:23:50 -0500

    ajustes heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { CategoriesModule } from './categories/categories.module';
+import { ServeStaticModule} from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports:[
     TypeOrmModule.forRoot(),
     UsuariosModule,
-    CategoriesModule
+    CategoriesModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,  '..','avatars')
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
+
